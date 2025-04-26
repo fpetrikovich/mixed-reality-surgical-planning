dc09aeb2b26dd114e7324127f4259ef4837a4f8b
Florencia Petrikovich <fpetrikovich@gmail.com>
2025-04-26 15:02:55 +0100

    :art: Apii url is now a constant

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CONFIGURATION } from './constants';
 
 
 function App() {
-  // const apiUrl = `http://localhost:${process.env.REACT_APP_BACKEND_PORT}`;
-  console.log(import.meta.env);
-  // const CONFIG_MAP = {
-  //   PORT: parseInt(import.meta.env.VITE_FRONTEND_PORT || "3000"),
-  //   BACKEND_PORT: parseInt(import.meta.env.VITE_BACKEND_PORT || "8000"),
-  //   API_URL: import.meta.env.VITE_API_URL
-  //     ? `http://localhost:${import.meta.env.VITE_BACKEND_PORT || "8000"}`
-  //     : "http://localhost:8000",
-  // };
-
-  // useEffect(() => {
-  //   console.log('Fetching data from API...');
-  //   fetch(`${apiUrl}/api/users`)
-  //     .then(res => res.json())
-  //     .then(data => console.log(data.message));
-  // }, []);
+  useEffect(() => {
+    console.log('Fetching data from API...');
+    fetch(`${CONFIGURATION.API_URL}/api/users`)
+      .then(res => res.json())
+      .then(data => console.log(data.message));
+  }, []);
   
   const [count, setCount] = useState(0)
 
---
+++ frontend/src/constants.ts
@@ -0,0 +1,7 @@
+
+export const CONFIGURATION = {
+    API_URL: import.meta.env.VITE_BACKEND_PORT
+        ? `http://localhost:${import.meta.env.VITE_BACKEND_PORT}`
+        : "http://localhost:8000",
+    PORT: parseInt(import.meta.env.VITE_FRONTEND_PORT || "3000"),
+}
