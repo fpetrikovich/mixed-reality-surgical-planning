49ee5a310998d22e5cca11548a718f2756f82fa1
Florencia Petrikovich <fpetrikovich@gmail.com>
2025-04-26 14:59:32 +0100

    :wrench: Use vite as engine

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,53 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const apiUrl = `http://localhost:${process.env.REACT_APP_BACKEND_PORT}`;
 
 function App() {
-  useEffect(() => {
-    console.log('Fetching data from API...');
-    fetch(`${apiUrl}/api/users`)
-      .then(res => res.json())
-      .then(data => console.log(data.message));
-  }, []);
+  // const apiUrl = `http://localhost:${process.env.REACT_APP_BACKEND_PORT}`;
+  console.log(import.meta.env);
+  // const CONFIG_MAP = {
+  //   PORT: parseInt(import.meta.env.VITE_FRONTEND_PORT || "3000"),
+  //   BACKEND_PORT: parseInt(import.meta.env.VITE_BACKEND_PORT || "8000"),
+  //   API_URL: import.meta.env.VITE_API_URL
+  //     ? `http://localhost:${import.meta.env.VITE_BACKEND_PORT || "8000"}`
+  //     : "http://localhost:8000",
+  // };
+
+  // useEffect(() => {
+  //   console.log('Fetching data from API...');
+  //   fetch(`${apiUrl}/api/users`)
+  //     .then(res => res.json())
+  //     .then(data => console.log(data.message));
+  // }, []);
+  
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-      </header>
-    </div>
-  );
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the
+  // `VITE_` prefix.
+  let env = process.env;
+  if (mode !== "production") {
+    env = loadEnv(mode, process.cwd(), "");
+  }
+  const port = env.VITE_FRONTEND_PORT ? parseInt(env.VITE_FRONTEND_PORT) : 3000;
+  return {
+    server: {
+      port: port
+    },
+    // https://vite.dev/config/preview-options#preview-port
+    preview: {
+      port: port,
+    },
+    plugins: [react()],
+  };
+});
